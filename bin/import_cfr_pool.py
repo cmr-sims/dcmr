16a4401d6882bc08cc042f6f48258628e6622dea
Neal Morton <mortonne@gmail.com>
2020-05-17 17:15:39 -0500

    feat: sort imported cfr images to match semantic vectors

--- bin/import_cfr_pool.py
+++ bin/import_cfr_pool.py
@@ -4,20 +4,30 @@
 
 
 import argparse
+import numpy as np
+import pandas as pd
 from cfr import task
 
 
-def main(image_dir, output_dir, pool_file):
+def main(image_dir, map_file, output_dir, pool_file):
     pool = task.read_pool_cfr(image_dir)
     task.save_pool_images(pool, output_dir)
     pool = pool.drop(columns=['filepath'])
-    pool.to_csv(pool_file, index=False)
+
+    # sort to match map order
+    item_map = pd.read_csv(map_file)
+    pool_sorted = pool.copy()
+    for i, item in item_map['item'].iteritems():
+        pool_ind = pool['item'].str.upper().to_numpy() == item.upper()
+        pool_sorted.iloc[i, :] = pool.iloc[np.where(pool_ind)[0][0], :]
+    pool_sorted.to_csv(pool_file, index=False)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('image_dir', help="Path to directory with pool images")
+    parser.add_argument('map_file', help="Path to pool map file")
     parser.add_argument('output_dir', help="Path to output directory")
     parser.add_argument('pool_file', help="Path to output pool CSV file")
     args = parser.parse_args()
-    main(args.image_dir, args.output_dir, args.pool_file)
+    main(args.image_dir, args.map_file, args.output_dir, args.pool_file)
