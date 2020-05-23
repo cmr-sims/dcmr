1a791782ee7d6ea5aa56feb7586c21239902a02b
Neal Morton <mortonne@gmail.com>
2020-05-23 02:48:42 -0500

    feat: update fit function to support model variants

--- bin/cfr_fit_cmr.py
+++ bin/cfr_fit_cmr.py
@@ -2,31 +2,33 @@
 #
 # Fit CMR to CFR data.
 
-import os
 import argparse
 import pandas as pd
 from cymr import models
+from cymr import network
+from cfr import framework
 
 
-def main(data_file, res_dir, n_jobs=1):
+def main(data_file, patterns_file, model_type, sem_model, res_file, n_jobs=1):
 
     data = pd.read_csv(data_file)
-    model = models.CMR()
-    fixed = {'B_rec': .8, 'L': 1, 'T': 10, 'X1': .05, 'X2': 1}
-    var_names = ['B_enc', 'B_rec']
-    var_bounds = {'B_enc': (0, 1), 'B_rec': (0, 1)}
-    results = model.fit_indiv(data, fixed, var_names, var_bounds, n_jobs=n_jobs)
-
-    if not os.path.exists(res_dir):
-        os.makedirs(res_dir)
-    res_file = os.path.join(res_dir, 'cfr_cmr_fit.csv')
+    model = models.CMRDistributed()
+    wp = framework.model_variant(model_type, sem_model)
+    patterns = network.load_patterns(patterns_file)
+    results = model.fit_indiv(data, wp.fixed, wp.free, wp.dependent,
+                              patterns=patterns, weights=wp.weights,
+                              n_jobs=n_jobs, method='de')
     results.to_csv(res_file)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('data_file')
-    parser.add_argument('res_dir')
+    parser.add_argument('patterns_file')
+    parser.add_argument('model_type')
+    parser.add_argument('sem_model')
+    parser.add_argument('res_file')
     parser.add_argument('--n-jobs', '-j', type=int, default=1)
     args = parser.parse_args()
-    main(args.data_file, args.res_dir, args.n_jobs)
+    main(args.data_file, args.patterns_file, args.model_type, args.sem_model,
+         args.res_file, args.n_jobs)
