177e62d2155eef68abb2a8b7692ecee7cc366e7f
Neal Morton <mortonne@gmail.com>
2020-06-07 00:26:43 -0500

    feat: option to set number of replications to simulate

--- bin/cfr_submit_fit_cmr.py
+++ bin/cfr_submit_fit_cmr.py
@@ -7,7 +7,8 @@ import argparse
 import numpy as np
 
 
-def main(fcf_features, ff_features, res_dir, n_rep=10, n_job=48, tol=0.00001):
+def main(fcf_features, ff_features, res_dir, n_rep=10, n_job=48, tol=0.00001,
+         n_sim_rep=50):
     study_dir = os.environ['STUDYDIR']
     if not study_dir:
         raise EnvironmentError('STUDYDIR not defined.')
@@ -15,7 +16,7 @@ def main(fcf_features, ff_features, res_dir, n_rep=10, n_job=48, tol=0.00001):
     data_file = os.path.join(study_dir, 'cfr', 'cfr_eeg_mixed.csv')
     patterns_file = os.path.join(study_dir, 'cfr', 'cfr_patterns.hdf5')
     inputs = f'{data_file} {patterns_file}'
-    opts = f'-t {tol:.6f} -n {n_rep} -j {n_job}'
+    opts = f'-t {tol:.6f} -n {n_rep} -j {n_job} -r {n_sim_rep}'
 
     res_name = 'cmr'
     if fcf_features and fcf_features != 'none':
@@ -35,6 +36,7 @@ if __name__ == '__main__':
     parser.add_argument('--n-rep', '-n', default=10, type=int)
     parser.add_argument('--n_job', '-j', default=48, type=int)
     parser.add_argument('--tol', '-t', type=float, default=0.00001)
+    parser.add_argument('--n-sim-rep', '-r', type=int, default=1)
     args = parser.parse_args()
 
     fcf_list = args.fcf_features.split(',')
@@ -47,4 +49,5 @@ if __name__ == '__main__':
         ff_list *= max_n
 
     for fcf, ff in zip(fcf_list, ff_list):
-        main(fcf, ff, args.res_dir, args.n_rep, args.n_job, args.tol)
+        main(fcf, ff, args.res_dir, args.n_rep, args.n_job, args.tol,
+             args.n_sim_rep)
