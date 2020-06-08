b70c68762eefabb3d1f1ff664c3df5273922c217
Neal Morton <mortonne@gmail.com>
2020-06-07 22:00:19 -0500

    refactor: add plot function keyword input

--- src/cfr/figures.py
+++ src/cfr/figures.py
@@ -4,7 +4,8 @@ import os
 import seaborn as sns
 
 
-def plot_fit(data, group_var, stat_name, f_stat, stat_kws, f_plot, out_dir):
+def plot_fit(data, group_var, stat_name, f_stat, stat_kws, f_plot, plot_kws,
+             out_dir):
     """Plot fit for an analysis and save figures."""
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -13,12 +14,12 @@ def plot_fit(data, group_var, stat_name, f_stat, stat_kws, f_plot, out_dir):
 
     # mean stat
     stat = data.groupby(group_var).apply(f_stat, **stat_kws)
-    g = f_plot(stat, hue=group_var, palette=palette, height=4)
+    g = f_plot(stat, hue=group_var, palette=palette, height=4, **plot_kws)
     g.savefig(os.path.join(out_dir, f'{stat_name}.pdf'))
 
     # subject stats
     g = f_plot(stat, hue=group_var, palette=palette, col='subject',
-               col_wrap=6, height=3)
+               col_wrap=6, height=3, **plot_kws)
     g.savefig(os.path.join(out_dir, f'{stat_name}_subject.pdf'))
 
     # comparison scatter plot
