bf293741c96cdfd15a9cdba49e58f90f19a0f9d1
Neal Morton <mortonne@gmail.com>
2020-06-21 10:02:54 -0500

    feat: additional plots for distance rank, pfr

--- bin/cfr_plot_fit.py
+++ bin/cfr_plot_fit.py
@@ -6,7 +6,6 @@ import os
 import argparse
 import numpy as np
 import pandas as pd
-import seaborn as sns
 from psifr import fr
 from cymr import network
 from cfr import task
@@ -31,37 +30,66 @@ def main(data_file, patterns_file, fit_dir):
     # make plots
     fig_dir = os.path.join(fit_dir, 'figs')
 
-    # temporal rank
-    p = full.groupby('source').apply(fr.lag_rank)
-    comp = p[['rank']].unstack(level=0).droplevel(axis=1, level=0)
-    g = sns.relplot(kind='scatter', x='Data', y='Model',
-                    data=comp.reset_index())
-    g.axes[0, 0].plot([0, 1], [0, 1], '-k')
-    g.savefig(os.path.join(fig_dir, 'lag_rank_comp.pdf'))
+    # scalar stats
+    figures.plot_fit_scatter(
+        full, 'source', 'use_rank', fr.distance_rank,
+        {'index_key': 'item_index', 'distances': rsm}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'use_rank_within', fr.distance_rank,
+        {'index_key': 'item_index', 'distances': rsm,
+         'test_key': 'category', 'test': lambda x, y: x == y}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'use_rank_across', fr.distance_rank,
+        {'index_key': 'item_index', 'distances': rsm,
+         'test_key': 'category', 'test': lambda x, y: x != y}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'lag_rank', fr.lag_rank, {}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'lag_rank_within', fr.lag_rank,
+        {'test_key': 'category', 'test': lambda x, y: x == y}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'lag_rank_across', fr.lag_rank,
+        {'test_key': 'category', 'test': lambda x, y: x != y}, 'rank', fig_dir
+    )
+    figures.plot_fit_scatter(
+        full, 'source', 'cat_crp', fr.category_crp,
+        {'category_key': 'category'}, 'prob', fig_dir
+    )
 
-    # category clustering
-    p = full.groupby('source').apply(fr.category_crp, category_key='category')
-    comp = p[['prob']].unstack(level=0).droplevel(axis=1, level=0)
-    g = sns.relplot(kind='scatter', x='Data', y='Model',
-                    data=comp.reset_index())
-    g.axes[0, 0].plot([0, 1], [0, 1], '-k')
-    g.savefig(os.path.join(fig_dir, 'cat_crp_comp.pdf'))
-
+    # curves
     figures.plot_fit(
         full, 'source', 'use_crp',
-        lambda x: fr.distance_crp(x, 'item_index', rsm, edges),
-        {}, fr.plot_distance_crp, {'min_samples': 10}, fig_dir
+        lambda x: fr.distance_crp(x, 'item_index', rsm, edges), {}, 'prob',
+        fr.plot_distance_crp, {'min_samples': 10}, fig_dir
     )
-    figures.plot_fit(full, 'source', 'spc', fr.spc, {}, fr.plot_spc, {},
-                     fig_dir)
-    figures.plot_fit(full, 'source', 'lag_crp', fr.lag_crp, {},
-                     fr.plot_lag_crp, {}, fig_dir)
-    figures.plot_fit(full, 'source', 'lag_crp_within', fr.lag_crp,
-                     {'test_key': 'category', 'test': lambda x, y: x == y},
-                     fr.plot_lag_crp, {}, fig_dir)
-    figures.plot_fit(full, 'source', 'lag_crp_across', fr.lag_crp,
-                     {'test_key': 'category', 'test': lambda x, y: x != y},
-                     fr.plot_lag_crp, {}, fig_dir)
+    figures.plot_fit(
+        full, 'source', 'spc', fr.spc, {}, 'recall',
+        fr.plot_spc, {}, fig_dir
+    )
+    figures.plot_fit(
+        full, 'source', 'pfr',
+        lambda x: fr.pnr(x).query('output == 1'), {}, 'prob',
+        fr.plot_spc, {}, fig_dir
+    )
+    figures.plot_fit(
+        full, 'source', 'lag_crp', fr.lag_crp, {}, 'prob',
+        fr.plot_lag_crp, {}, fig_dir
+    )
+    figures.plot_fit(
+        full, 'source', 'lag_crp_within', fr.lag_crp,
+        {'test_key': 'category', 'test': lambda x, y: x == y},
+        'prob', fr.plot_lag_crp, {}, fig_dir
+    )
+    figures.plot_fit(
+        full, 'source', 'lag_crp_across', fr.lag_crp,
+        {'test_key': 'category', 'test': lambda x, y: x != y},
+        'prob', fr.plot_lag_crp, {}, fig_dir
+    )
 
 
 if __name__ == '__main__':
