d6b970bbfd2c05d70e4f0f65c34a1c08f35eb8ab
Neal Morton <mortonne@gmail.com>
2020-12-14 13:56:09 -0600

    feat: support for multiple classifiers

--- bin/cfr_decode_eeg.py
+++ bin/cfr_decode_eeg.py
@@ -11,26 +11,26 @@ from cfr import task
 from cfr import decode
 
 
-def decode_subject(patterns_dir, subject):
+def decode_subject(patterns_dir, subject, clf='svm'):
     pattern_file = os.path.join(patterns_dir, f'sub-{subject}_pattern.txt')
     events_file = os.path.join(patterns_dir, f'sub-{subject}_events.csv')
     pattern = np.loadtxt(pattern_file)
     events = pd.read_csv(events_file)
 
     pattern = decode.impute_samples(pattern)
-    evidence = decode.classify_patterns(events, pattern)
+    evidence = decode.classify_patterns(events, pattern, clf=clf)
 
     out_file = os.path.join(patterns_dir, f'sub-{subject}_decode.csv')
     df = pd.concat([events, evidence], axis=1)
     df.to_csv(out_file)
 
 
-def main(patterns_dir, n_jobs=1, subjects=None):
+def main(patterns_dir, n_jobs=1, subjects=None, clf=None):
     if subjects is None:
         subjects, _ = task.get_subjects()
 
     Parallel(n_jobs=n_jobs)(
-        delayed(decode_subject)(patterns_dir, subject) for subject in subjects
+        delayed(decode_subject)(patterns_dir, subject, clf) for subject in subjects
     )
 
 
@@ -46,10 +46,13 @@ if __name__ == '__main__':
     parser.add_argument(
         '--subjects', '-s', default=None, help="Comma-separated list of subjects."
     )
+    parser.add_argument(
+        '--classifier', '-c', default='svm', help="classifier type."
+    )
     args = parser.parse_args()
 
     if args.subjects is not None:
         inc_subjects = [f'LTP{subject:0>3}' for subject in args.subjects.split(',')]
     else:
         inc_subjects = None
-    main(args.patterns_dir, args.n_jobs, inc_subjects)
+    main(args.patterns_dir, args.n_jobs, inc_subjects, args.classifier)
