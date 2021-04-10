ebfad77584e257f2e5c20f24d1b36e252675f3d3
Neal Morton <mortonne@gmail.com>
2021-04-10 05:33:54 -0500

    refactor: require source and destination as input

--- bin/cfr_pull_fits.sh
+++ bin/cfr_pull_fits.sh
@@ -2,13 +2,18 @@
 #
 # Pull fit results from TACC.
 
-rsync -azvu stampede:work/cmr_cfr/cfr/fits/ \
-  "$STUDYDIR"/cfr/fits \
+src=$1
+dest=$2
+shift 2
+
+rsync -azvu "$src/cfr/fits/" \
+  "$dest/cfr/fits" \
   --include="*/" \
   --include="*/cmr*/" \
   --include="*/cmr*/figs/" \
   --include="*.csv" \
   --include="*.json" \
   --include="*.pdf" \
+  --include="*.txt" \
   --exclude="*" \
   "$@"
