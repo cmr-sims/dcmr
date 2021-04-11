80a6f957d113b299d8ef35d4f263e313ca3ad6bc
Neal Morton <mortonne@gmail.com>
2021-04-11 14:46:53 -0500

    feat: move css files to hidden directory

--- src/cfr/figures.py
+++ src/cfr/figures.py
@@ -132,10 +132,10 @@ def render_fit_html(fit_dir, curves, points, ext='svg'):
 
     # copy css
     css = env.get_template('bootstrap.min.css')
-    css_file = os.path.join(fit_dir, 'bootstrap.min.css')
+    css_file = os.path.join(fit_dir, '.css', 'bootstrap.min.css')
     with open(css_file, 'w') as f:
         f.write(css.render())
     css = env.get_template('report.css')
-    css_file = os.path.join(fit_dir, 'report.css')
+    css_file = os.path.join(fit_dir, '.css', 'report.css')
     with open(css_file, 'w') as f:
         f.write(css.render())
