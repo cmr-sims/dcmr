a29be0f50b52c7d382662905982f6038822c993c
Neal Morton <mortonne@gmail.com>
2021-05-10 11:52:12 -0500

    style: standardize tab size

--- bin/cfr_pull_fits.sh
+++ bin/cfr_pull_fits.sh
@@ -13,17 +13,17 @@ dest=$2
 shift 2
 
 rsync -azvu "$src/cfr/fits/" \
-  "$dest/cfr/fits" \
-  --include="*/" \
-  --include="*/cmr*/" \
-  --include="*/cmr*/figs/" \
-  --include="*/cmr*/.css" \
-  --include="*.csv" \
-  --include="*.json" \
-  --include="*.pdf" \
-  --include="*.txt" \
-  --include="*.css" \
-  --include="*.html" \
-  --include="*.svg" \
-  --exclude="*" \
-  "$@"
+    "$dest/cfr/fits" \
+    --include="*/" \
+    --include="*/cmr*/" \
+    --include="*/cmr*/figs/" \
+    --include="*/cmr*/.css" \
+    --include="*.csv" \
+    --include="*.json" \
+    --include="*.pdf" \
+    --include="*.txt" \
+    --include="*.css" \
+    --include="*.html" \
+    --include="*.svg" \
+    --exclude="*" \
+    "$@"
