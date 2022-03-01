f108355dc35a7640338d75f8a930423b4be6082b
Neal Morton <mortonne@gmail.com>
2022-03-01 00:36:48 -0600

    test: class predicted probabilities

--- tests/test_logreg.py
+++ tests/test_logreg.py
@@ -41,3 +41,25 @@ def test_logreg1(patterns):
     max_rounds = 5000
     w, ll, n = decode.logistic_regression(x, y, l, tol, max_rounds)
     np.testing.assert_allclose(w, expected, atol=0.0001)
+
+
+def test_prob(patterns):
+    expected = [
+        [0.4936, 0.5165, 0.4898],
+        [0.4767, 0.5264, 0.5107],
+        [0.5361, 0.4842, 0.4746],
+        [0.5205, 0.4816, 0.5003],
+        [0.4610, 0.5434, 0.5016],
+        [0.4263, 0.6165, 0.4791],
+    ]
+    train = patterns['chunks'] == 1
+    y = patterns['labels'][train]
+    X = patterns['vectors'][train, :]
+
+    clf = decode.LogReg(l=10)
+    clf.fit(X, y)
+
+    test = patterns['chunks'] == 2
+    X_ = patterns['vectors'][test, :]
+    prob = clf.predict_proba(X_)
+    np.testing.assert_allclose(prob, expected, atol=0.0001)
