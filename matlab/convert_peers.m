153272a9309fdc0be35e502ac0471d89b73bd5ef
Neal Morton <mortonne@gmail.com>
2022-06-23 00:44:59 -0500

    feat: matlab script to convert peers data

---
+++ matlab/convert_peers.m
@@ -0,0 +1,11 @@
+
+addpath('/Users/morton/PycharmProjects/psifr/matlab');
+
+data_file = '/Users/morton/matlab/cmr_bak/cmr_branch/fr/TFRLTP/data/peers_e1_data.mat';
+load(data_file);
+
+subs = trial_subset(data.listtype == -1, data, 1);
+tab = frdata2table(subs);
+
+out_file = '/Users/morton/Dropbox/work/cmr_cfr/peers/peers_notask.csv';
+writetable(tab, out_file);
