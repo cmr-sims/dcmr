1756c6afd930c6cf59d0dc85c0cf83a11e9958f9
Neal Morton <mortonne@gmail.com>
2022-06-23 02:04:25 -0500

    feat: add session column to peers data

--- matlab/convert_peers.m
+++ matlab/convert_peers.m
@@ -6,6 +6,7 @@ load(data_file);
 
 subs = trial_subset(data.listtype == -1, data, 1);
 tab = frdata2table(subs);
+tab.session = ceil(tab.list / 4);
 
 out_file = '/Users/morton/Dropbox/work/cmr_cfr/peers/peers_notask.csv';
 writetable(tab, out_file);
