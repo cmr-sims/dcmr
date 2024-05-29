8e09647b52816c33fc5ee29eb3d65076a55428a4
Neal Morton <mortonne@gmail.com>
2024-05-29 14:14:11 -0500

    feat: script to plan all simulation analysis commands

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import setuptools
 import glob
 
-scripts = glob.glob('bin/*.py') + glob.glob('bin/*.sh')
+scripts = glob.glob('bin/*.py') + glob.glob('bin/*.sh') + ['bin/dcmr-plan']
 setuptools.setup(scripts=scripts)
