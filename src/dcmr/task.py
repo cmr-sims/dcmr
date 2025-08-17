df7ca91d08ab425df3faac6c0ce0fbedb868ae09
Neal Morton <mortonne@gmail.com>
2025-08-17 10:07:10 -0500

    feat: support for additional keywords

--- src/dcmr/task.py
+++ src/dcmr/task.py
@@ -478,16 +478,16 @@ def category_context_crp(
     return crp
 
 
-def block_lag_crp(df, block_key, n_block_key=None):
+def block_lag_crp(df, block_key, n_block_key=None, **kwargs):
     """Conditional response probability by block lag."""
     if n_block_key is not None:
         crp = df.groupby(n_block_key).apply(
-            fr.lag_crp, lag_key=block_key, count_unique=True
+            fr.lag_crp, lag_key=block_key, count_unique=True, **kwargs
         ).reset_index()
         if n_block_key != 'n_block':
             crp = crp.rename(columns={n_block_key: 'n_block'})
     else:
-        crp = fr.lag_crp(df, lag_key=block_key, count_unique=True)
+        crp = fr.lag_crp(df, lag_key=block_key, count_unique=True, **kwargs)
     crp = crp.rename(columns={block_key: 'block'})
     return crp
 
